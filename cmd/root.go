62b570bf42bfe0b02c0a897a542efdaa11865188
Kiran <116376429+kiranvshah@users.noreply.github.com>
2022-12-19 23:00:41 +0000

    add descriptions

--- cmd/root.go
+++ cmd/root.go
@@ -11,14 +11,13 @@ import (
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "go-noughts-and-crosses",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Use:   "noughts-and-crosses",
+	Short: "Play noughts and crosses on the command line",
+	Long: `A CLI to play noughts and crosses, written in Go.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+Usage:
+  noughts-and-crosses [command]
+TODO: put more detailed instructions here once the app is built`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
