a8985c85bc087ab7dc69763a6690d55af6af72ac
Kiran <116376429+kiranvshah@users.noreply.github.com>
2022-12-21 18:35:51 +0000

    add version number to release binaries

--- build_and_release.sh
+++ build_and_release.sh
@@ -1,9 +1,10 @@
-rm -rf binaries
-GOOS=darwin GOARCH=amd64 go build -o binaries/go-noughts-and-crosses-darwin-amd64
-GOOS=linux GOARCH=amd64 go build -o binaries/go-noughts-and-crosses-linux-amd64
-GOOS=windows GOARCH=amd64 go build -o binaries/go-noughts-and-crosses-windows-amd64.exe
-
 read -p "Enter version number (e.g. 2.3.4): " version
+
+rm -rf binaries
+GOOS=darwin GOARCH=amd64 go build -o "binaries/go-noughts-and-crosses-v$version-darwin-amd64"
+GOOS=linux GOARCH=amd64 go build -o "binaries/go-noughts-and-crosses-v$version-linux-amd64"
+GOOS=windows GOARCH=amd64 go build -o "binaries/go-noughts-and-crosses-v$version-windows-amd64.exe"
+
 git tag "v$version"
 git push origin "v$version"
 gh release create "v$version" ./binaries/* --title "v$version" --generate-notes
